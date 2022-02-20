6b11dea31f0775c199ace5651b0e3af9fd38cd16
Sofia Lozada <sofia@ioio.mx>
2022-02-20 13:37:39 -0600

    fieldtypes

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 // in src/App.js
 import * as React from "react";
-import { Admin } from "react-admin";
+import { Admin, Resource } from "react-admin";
+import { UserList } from "./users";
 import jsonServerProvider from "ra-data-json-server";
 
 const dataProvider = jsonServerProvider("https://jsonplaceholder.typicode.com");
-const App = () => <Admin dataProvider={dataProvider} />;
+const App = () => (
+  <Admin dataProvider={dataProvider}>
+    <Resource name="users" list={UserList} />
+  </Admin>
+);
 
 export default App;
