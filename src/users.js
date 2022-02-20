af29c76ce1e3d5325d405405035b71cf8ffb2c00
Sofia Lozada <sofia@ioio.mx>
2022-02-20 13:40:44 -0600

    customizinstyles

--- src/users.js
+++ src/users.js
@@ -1,6 +1,7 @@
 // in src/users.js
 import * as React from "react";
 import { List, Datagrid, TextField, EmailField } from "react-admin";
+import MyUrlField from "./MyUrlField";
 
 export const UserList = (props) => (
   <List {...props}>
@@ -9,7 +10,7 @@ export const UserList = (props) => (
       <TextField source="name" />
       <EmailField source="email" />
       <TextField source="phone" />
-      <TextField source="website" />
+      <MyUrlField source="website" />
       <TextField source="company.name" />
     </Datagrid>
   </List>
